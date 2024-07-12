992c9fd479edfc169daa4be76265e837ec425059
Kleber Lucas <kleber.llucas2013@gmail.com>
2024-07-11 23:21:56 -0300

    ESSENCIAL 1

--- sql/plsql.sql
+++ sql/plsql.sql
@@ -1,1 +1,14 @@
+CREATE OR REPLACE PROCEDURE brh.insere_projeto
+(
+    p_NOME IN brh.projeto.nome%type,
+    p_RESPONSAVEL IN brh.projeto.responsavel%type   
+)
 
+IS
+
+BEGIN
+    INSERT INTO brh.projeto (NOME, RESPONSAVEL, INICIO) VALUES (p_NOME, UPPER(p_RESPONSAVEL), SYSDATE);
+    
+    COMMIT; --NECESSARIO NO PL SQL
+
+END;
